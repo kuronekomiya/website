7499f816cfe1721f4800d6e2393d4f50a028caf7
Mia Perez <miaperez@Mias-MacBook-Air-303.local>
2026-06-06 10:57:49 +0800

    new post and about section

--- js/blog_footer.js
+++ js/blog_footer.js
@@ -3,20 +3,11 @@ class FooterComponent extends HTMLElement {
     this.innerHTML = `
       <footer>
         <div>
-          <p> Hey, thanks for reading my writing! A lot of what I publish on this website was written by hand while waiting for the bus or having my morning coffee. It is far from perfect and I try to edit as little as posible. I'm trying to capture thinking-as-it-happens.</p>
+          <p>Hey, thanks for reading my writing! I am approximately one thousand rats living and working in Beijing. </br>A lot of what I publish on this website was first written by hand. It is far from perfect and I try to edit as little as posible, but I hope you've enjoyed it!</p>
         </div>
-        <nav>
-          <ul>
-            <li><a href="blog_index.html">Home</a></li>
-            <li><a href="/blog_essays.html">Essays</a></li>
-            <li><a href="/blog_weekly.html">Weekly</a></li>
-            <li><a href="/blog_about.html">About</a></li>
-            <li><a href="/blog_archive.html">Misc./archive</a></li>
-          </ul>
-        </nav>
       </footer>
-    `;
-  }
+    ;
+  `}
 }
 
 customElements.define('footer-component', FooterComponent);

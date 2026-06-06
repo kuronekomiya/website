8baacbe55c05d334eb89de2535825dbbfc99c560
Mia Perez <miaperez@Mias-MacBook-Air-303.local>
2026-06-06 10:59:34 +0800

    Updates

--- js/blog_header.js
+++ js/blog_header.js
@@ -6,10 +6,10 @@ class HeaderComponent extends HTMLElement {
           <div class="logo"><a href="blog_index.html"></a>NekoMiya</div>
           <ul>
             <li><a href="blog_index.html">Home</a></li>
-            <li><a href="/blog_essays.html">Essays</a></li>
-            <li><a href="/blog_weekly.html">Weekly</a></li>
-            <li><a href="/blog_about.html">About</a></li>
-            <li><a href="/blog_archive.html">Misc./archive</a></li>
+            <!-- <li><a href="/blog_essays.html">Essays</a></li>
+            <li><a href="/blog_weekly.html">Weekly</a></li> -->
+            <li><a href="blog_about.html">About</a></li>
+            <!-- <li><a href="/blog_archive.html">Misc./archive</a></li>-->
           </ul>
         </nav>
       </header>
